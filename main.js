24d44329284a2d75b20431827a2d531bda73c1c9
Sunkay <dieyfnkg@qq.com>
2018-06-04 16:44:58 +0800

    换图片

--- main.js
+++ main.js
@@ -4,36 +4,36 @@ function random(){
      var number = parseInt(1000*Math.random())%10;
      if (number == 0)
      {
-        img.src = "./img/test1.png";
+        img.src = "./img/test1.jpeg";
      }
      if (number == 1)
      {
-         img.src = "./img/test2.png";
+         img.src = "./img/test2.jpeg";
      }
      if (number == 2)
      {
-         img.src = "./img/test3.png";
+         img.src = "./img/test3.jpeg";
      }
      if (number == 3){
-         img.src = "./img/test4.png";
+         img.src = "./img/test4.jpeg";
      }
      if (number == 4){
-         img.src = "./img/test5.png";
+         img.src = "./img/test5.jpeg";
      }
      if (number == 5){
-         img.src = "./img/test6.png";
+         img.src = "./img/test6.jpeg";
      }
      if (number == 6){
-         img.src = "./img/test7.png";
+         img.src = "./img/test7.jpeg";
      }
      if (number == 7){
-         img.src = "./img/test8.png";
+         img.src = "./img/test8.jpeg";
      }
      if (number == 8){
-         img.src = "./img/test9.png";
+         img.src = "./img/test9.jpeg";
      }
      if (number == 9){
-         img.src = "./img/test10.png";
+         img.src = "./img/test10.jpeg";
      }
      console.log(number);
 
